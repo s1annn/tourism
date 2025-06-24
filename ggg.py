6f494bc7d71af0e02001a9a49328e84ea1e7c455
s1annn <mrsyan132007@gmail.com>
2025-06-24 20:17:57 +0200

    Create base class Place
    Add abstract Place class with emoji attribute

--- ggg.py
+++ ggg.py
@@ -1,3 +1,13 @@
 import tkinter as tk
 from tkinter import messagebox, ttk
 from abc import ABC, abstractmethod
+
+class Place(ABC):
+    def __init__(self, name, cost, experience, emoji):
+         self.name = name
+         self.cost = cost
+         self.experience = experience
+         self.emoji = emoji
+
+    def describe(self):
+      pass
