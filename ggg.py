4fb3bd1c205db15d5961ea26f0434f9bbec7da80
s1annn <mrsyan132007@gmail.com>
2025-06-24 20:21:06 +0200

    Implement places
    Implement Beach, Mountain, and City classes as Place subclasses

--- ggg.py
+++ ggg.py
@@ -11,3 +11,16 @@ class Place(ABC):
 
     def describe(self):
       pass
+#Classes for different places
+class Beach(Place):
+    def describe(self):
+        return f"{self.emoji} {self.name}' - Chill and enjoy the sun. Cost : {self.cost}, EXP : {self.experience}"
+
+class Mountain(Place):
+    def describe(self):
+        return f"{self.emoji} {self.name}' - Enjoy beautiful views! Cost : {self.cost}, EXP: {self.experience}"
+
+class City(Place):
+    def describe(self):
+         return f"{self.emoji} {self.name}' - Culture, shopping and exiting places. Cost: {self.cost}, EXP : {self.experience}"
+
